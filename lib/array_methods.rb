4f542364d6fcfe0b7088fa8653b6321bcbb6df14
Ben Hickson <bzhickson@gmail.com>
2020-02-08 19:06:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,6 +4,7 @@ def find_element_index(array, value_to_find)
       return index
     end
   end
+  return nil #returns this if the explicit return above does not happen
 end
 
 def find_max_value(array)
